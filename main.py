0361e4919af18ad4c9ed193bfae5f7e839b66cd2
Fynn Kosnick <kofy95@live.com>
2023-06-23 22:03:46 +0000

    implement request

--- main.py
+++ main.py
@@ -24,8 +24,25 @@ URL = "https://www.arbeitstage.org/"
 
 
 def requestHolidays(year, state):
+    results = []
+    url = URL + f"{state}/feiertage-{year}-{state}/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find(attrs={"class": "tableJahrFeier"})
+    holiday_lines = table.findAll("tr")
+    holiday_lines.pop(0)
+    for line in holiday_lines:
+        tds = line.findAll("td")
+        results.append(tds[1].text[4:])
+    return results
+
+
+def formatDate(date):
     pass
 
 
 def exportResults():
     pass
+
+
+print(requestHolidays(2024, "bayern"))
